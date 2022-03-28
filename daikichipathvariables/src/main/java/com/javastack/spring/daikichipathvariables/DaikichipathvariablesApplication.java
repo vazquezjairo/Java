aa4e940ee4418fccd2f1430f4bec00805a47f474
vazquezjairo <91754203+vazquezjairo@users.noreply.github.com>
2022-03-28 08:44:24 -0700

    Add files via upload

---
+++ daikichipathvariables/src/main/java/com/javastack/spring/daikichipathvariables/DaikichipathvariablesApplication.java
@@ -0,0 +1,13 @@
+package com.javastack.spring.daikichipathvariables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichipathvariablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichipathvariablesApplication.class, args);
+	}
+
+}
